60daa0b8804ed0d18eec2c01c80e59287c19b82b
Anselmo Xavier <anselmo.cstecnologia@gmail.com>
2024-01-23 00:17:57 -0300

    finalizando frontEnd utilizando axios

--- public/index.js
+++ public/index.js
@@ -1,16 +1,57 @@
-
-
 async function carregarAutomoveis(){
   const response = await axios.get('http://127.0.0.1:8000/carros')
 
-  console.log(response.data)
+  const carros = response.data
+
+  const lista = document.getElementById('lista_carros')
+
+  lista.innerHTML= ''
+
+  carros.forEach(carro => { 
+    const item = document.createElement('li')
+    item.innerText = carro.nome 
+  
+    lista.appendChild(item)
+  });
+}
 
 
+function gravarFormulario() {
+  const carro = document.getElementById('form-carro');
+  const input_id = document.getElementById('id')
+  const input_nome = document.getElementById('nome')
+  const input_marca = document.getElementById('marca')
+  const input_modelo = document.getElementById('modelo')
+  const input_cor = document.getElementById('cor')
+  const input_preco = document.getElementById('preco')
+
+  carro.onsubmit = async (event) => {
+    event.preventDefault();
+    const id = input_id.value
+    const nome = input_nome.value
+    const marca = input_marca.value
+    const modelo = input_modelo.value
+    const cor = input_cor.value
+    const preco = input_preco.value
+
+    await axios.post('http://127.0.0.1:8000/carros',{
+        id: id,
+        nome: nome,
+        marca: marca,
+        modelo: modelo,
+        cor: cor,
+        preco: preco
+
+  })
+    carregarAutomoveis()
+    alert('Carro cadastrado com sucesso')
+  }
 }
 
 function app(){
-  console.log('Inciando api ....')
+  console.log('Iniciando api ....') 
   carregarAutomoveis()
+  gravarFormulario()
 }
 
-app()
+document.addEventListener('DOMContentLoaded', app);
